a74722b44f178819f1ff0c736c06ad8c2731bf85
Gabriel Azevedo <gpeto91@hotmail.com>
2025-02-01 21:12:57 -0300

    chore: atualizar link da API do radio

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const DEV_URL = "http://localhost:8000";
-export const PROD_URL = "http://18.231.20.132:8000";
+export const PROD_URL = "https://guarani-radio.ddns.net/";
 
 export const api = axios.create({ baseURL: PROD_URL });
