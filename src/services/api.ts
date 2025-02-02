7f43c1e90b0b11c6950eb919853cefce4beda6bf
Gabriel Azevedo <gpeto91@hotmail.com>
2025-02-01 22:06:21 -0300

    fix: remover barra da url da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const DEV_URL = "http://localhost:8000";
-export const PROD_URL = "https://guarani-radio.ddns.net/";
+export const PROD_URL = "https://guarani-radio.ddns.net";
 
 export const api = axios.create({ baseURL: PROD_URL });
